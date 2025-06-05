933a1fcf02b6be92f0d34e25de1d0f6da4752575
LaGodxy <algodicino@gmail.com>
2025-06-05 09:26:55 -0700

    auth module, and entity

--- backend/stark-cert-backend/src/app.module.ts
+++ backend/stark-cert-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { Auth } from './auth/entities/auth.entity';
 
 @Module({
   imports: [
--- backend/stark-cert-backend/src/auth/auth.module.ts
+++ backend/stark-cert-backend/src/auth/auth.module.ts
@@ -5,10 +5,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt-strategy';
 import { LocalStrategy } from './strategies/local-strategy';
+import { Auth } from './entities/auth.entity';
 
 @Module({
   imports: [
     PassportModule,
+    Auth,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
       signOptions: { expiresIn: '1h' },
